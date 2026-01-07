faca152a25d6c2aef9af275b9c944a42acb67fe4
iwasaki <poettype73@gmail.com>
2026-01-07 13:18:00 +0900

    Add content.config

---
+++ src/content.config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+
+const constructionTraining = defineCollection({
+  type: 'content',
+  schema: () =>
+    z.object({
+      title: z.string(),
+      section: z.string(),
+      order: z.number(),
+    }),
+});
+
+export const collections = { constructionTraining };
