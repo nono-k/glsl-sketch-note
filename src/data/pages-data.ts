927f2fcd1f3b24a90e03d0c7405ea22ba9217a9b
iwasaki <poettype73@gmail.com>
2026-01-19 09:32:30 +0900

    Add how to make repeat patterns page data

--- src/data/pages-data.ts
+++ src/data/pages-data.ts
@@ -5,4 +5,8 @@ export const pagesData: Pages[] = [
     title: '構築学のデザイントレーニング',
     url: '/construction-training',
   },
+  {
+    title: '幾何学パターンづくりのすべて',
+    url: '/how-to-make-repeat-patterns',
+  },
 ];
