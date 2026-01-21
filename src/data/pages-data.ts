7cd9c4f4647bca399bd770cadbf9512631bfee66
iwasaki <poettype73@gmail.com>
2026-01-22 00:36:30 +0900

    Add laws of decorative patterns page data

--- src/data/pages-data.ts
+++ src/data/pages-data.ts
@@ -2,6 +2,10 @@ import type { Pages } from '@/types/config';
 
 export const pagesData: Pages[] = [
   {
+    title: '装飾パターンの法則',
+    url: '/laws-of-decorative-patterns',
+  },
+  {
     title: '構築学のデザイントレーニング',
     url: '/construction-training',
   },
